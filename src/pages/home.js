62b04e406d6aeda587f1eea986f1f073f9720bd2
serkviseal <serkviseal15@gmail.com>
2021-05-25 19:08:28 +0300

    banner UI design

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Global from '../components/reusable/global';
+import HeaderBanner from './../components/views/banner/HeaderBanner';
 import FooterBanner from './../components/views/banner/FooterBanner';
 
 export class Home extends Component {
     render() {
         return (
             <Global>
-               <FooterBanner />
+                <HeaderBanner />
+                <FooterBanner />
             </Global>
         )
     }
